d1dfa67cabe538ca96c7d92e1e58063ae50ca1be
Alexander Yolkin <alexander.yolkin@gmail.com>
2019-02-19 11:25:27 +0200

    init

---
+++ Simulators/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Simulators
+//
+//  Created by Oleksandr Yolkin on 2/19/19.
+//  Copyright © 2019 Oleksandr Yolkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
